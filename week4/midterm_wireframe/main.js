5551d0d0185bd68a140cdeb61546a4f45e6f5ff8
njk326 <njk326@nyu.edu>
2023-03-07 00:56:48 -0500

    update + add images

--- week4/midterm_wireframe/main.js
+++ week4/midterm_wireframe/main.js
@@ -1,35 +1,47 @@
-const scroll = document.getElementById("image-scroll");
+	const scroll = document.getElementById("image-scroll");
 
-const mouseDown = e => {
-	scroll.dataset.mouseDown = e.clientX;
-}
+	//get cursor location on mouse click
+	const mouseDown = e => {
+		scroll.dataset.mouseDown = e.clientX;
+	}
 
-const mouseUp = () => {
-	scroll.dataset.mouseDown = "0";
-	scroll.dataset.prevPercent = scroll.dataset.percentage;
-}
+	//reset cursor position on mouse release
+	const mouseUp = () => {
+		scroll.dataset.mouseDown = "0";
+		scroll.dataset.prevPercent = scroll.dataset.percent;
+	}
 
-const mouseMove = e => {
-	if(scroll.dataset.mouseDown === "0") return;
+	const mouseMove = e => {
+		// if mouse is not clicked, ignore mouse movement
+		if(scroll.dataset.mouseDown === "0") return;
+		
+		//get mouse movement as a percent of the total screen width
+		const mouseDelta = parseFloat(scroll.dataset.mouseDown) - e.clientX,
+			maxDelta = window.innerWidth / 2;
 
-	const mouseDelta = parseFloat(scroll.dataset.mouseDown) - e.clientX,
-		maxDelta = window.innerWidth / 2;
+		const percent = (mouseDelta / maxDelta) * -100,
+			nextPercentMax = parseFloat(scroll.dataset.prevPercent) + percent, 
+			nextPercent = Math.max(Math.min(nextPercentMax, 0), -100);
 
-	const percent = (mouseDelta / maxDelta) * 100;
-	nextPercentMax = parseFloat(scroll.dataset.prevPercent) + percent, 
-	nextPercent = Math.max(Math.min(nextPercentMax, 0), -100);
+		//remember scroll position
+		scroll.dataset.percent = nextPercent;
 
-	scroll.dataset.percent = nextPercent;
+		scroll.animate({
+			transform: `translate(${nextPercent}%, -50%)`
+		}, {duration: 1200, fill: "forwards"});
+		
+		//parallax effect
+		for(const image of scroll.getElementsByClassName("image")) {
+			image.animate({
+			  objectPosition: `${100 + nextPercent}% center`
+			}, { duration: 100, fill: "forwards" });
+		}
+	}
 
-	scroll.animate({
-		transform: 'translate(${nextPercent}%, -50%)'
-	}, {duration: 1200, fill: "forwards"});
-}
-
-//touch events
-window.onmousedown = e => mouseDown(e);
-window.ontouchstart = e => mouseDown(e.touches[0]);
-window.onmouseup = e => mouseUp(e);
-window.ontouchend = e => mouseUp(e.touches[0]);
-window.onmousemove = e => mouseMove(e);
-window.ontouchmove = e => mouseMove(e.touches[0]);
+	//touch events
+	window.onmousedown = e => mouseDown(e);
+	window.ontouchstart = e => mouseDown(e.touches[0]);
+	window.onmouseup = e => mouseUp(e);
+	window.ontouchend = e => mouseUp(e.touches[0]);
+	window.onmousemove = e => mouseMove(e);
+	window.ontouchmove = e => mouseMove(e.touches[0]);
